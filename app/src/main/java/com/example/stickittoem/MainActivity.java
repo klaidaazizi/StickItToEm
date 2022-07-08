9bd5b21b73bed7aa38f14c9b5c448b854598728a
lauraculligan <culligan.l@northeastern.edu>
2022-07-08 19:37:42 -0400

    added login functionality and sent logged in username to main activity

--- app/src/main/java/com/example/stickittoem/MainActivity.java
+++ app/src/main/java/com/example/stickittoem/MainActivity.java
@@ -3,13 +3,22 @@ package com.example.stickittoem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        currentUser = getIntent().getStringExtra("currentUser");
+    }
+
+    public void selectImage(View view) {
+
     }
 
 
